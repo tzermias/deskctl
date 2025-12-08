46ca9e7c8490d371194be02e8af44711c7d5f503
Aris Tzermias <zacharias.tzermias@gmail.com>
2025-12-08 23:40:15 +0200

    Reject messages with incorrect length of data
    Messages received from the desk controller are 7 (or more) bytes long.
    Nevertheless, on some occasions `deskctl` receives a payload with
    incorrect data length (the length exceeds the length of the payload) but
    having a correct preamble (`0xf2`) and EoM (`0x7e`).
    Consequently, deskctl panics with "index out of range" runtime error.
    
    To fix this regression (until we decipher the payload in question),
    `isValidData` function fails when the length of data exceeds the length
    of the payload itself.
    
    Closes #9.

--- pkg/jiecang/jiecang.go
+++ pkg/jiecang/jiecang.go
@@ -308,13 +308,18 @@ func (j *Jiecang) characteristicReceiver(buf []byte) {
 // They should start with "f2f2", end with "7e" and te previous to last byte (which is a checksum) should not fail.
 func isValidData(buf []byte) bool {
 	// Check preamble and last byte
-	if buf[0] != 0xf2 || buf[1] != 0xf2 || buf[len(buf)-1] != 0x7e {
+	if buf[0] != 0xf2 || buf[1] != 0xf2 || buf[len(buf)-1] != 0x7e || len(buf) < 6 {
 		return false
 	}
 
 	// Calculate checksum and verify if its correct
 	data_type := int(buf[2])
 	data_len := int(buf[3])
+	// Length of the data should not exceed the length of the payload.
+	// Last two bytes should always be the checksum and EoM (Ox7e)
+	if data_len+3 >= len(buf)-2 {
+		return false
+	}
 	received_checksum := int(buf[len(buf)-2])
 
 	calc_checksum := data_type + data_len
--- pkg/jiecang/jiecang_test.go
+++ pkg/jiecang/jiecang_test.go
@@ -29,6 +29,11 @@ func TestIsValidData(t *testing.T) {
 			expectedResult: true,
 		},
 		{
+			name:           "Valid message for unit settings",
+			input:          []byte{0xf2, 0xf2, 0x0e, 0x01, 0x00, 0x0f, 0x7e},
+			expectedResult: true,
+		},
+		{
 			name:           "Message for height range with incorrect checksum",
 			input:          []byte{0xf2, 0xf2, 0x07, 0x04, 0x04, 0xf8, 0x02, 0x6c, 0x48, 0x7e},
 			expectedResult: false,
@@ -41,6 +46,16 @@ func TestIsValidData(t *testing.T) {
 		{
 			name:           "Message that doesn't end with 7e",
 			input:          []byte{0xf2, 0xf2, 0xca, 0xfe},
+			expectedResult: false,
+		},
+		{
+			name:           "Message with length less than 6 bytes",
+			input:          []byte{0xf2, 0xf2, 0x01, 0x00, 0x02, 0x7e},
+			expectedResult: false,
+		},
+		{
+			name:           "Message with incorrect length parameter",
+			input:          []byte{0xf2, 0xf2, 0x01, 0x03, 0x04, 0x7e},
 			expectedResult: false,
 		},
 	}
